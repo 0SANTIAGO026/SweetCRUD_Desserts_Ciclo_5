9d2d2d8dede7611ed26901034bb9b26412c33704
JesusMarcano <jesusmarcanu24@gmail.com>
2024-12-12 00:11:33 -0500

    adding create

--- src/main/java/com/project/web/SweetCRUD/service/impl/MaintenanceServiceImpl.java
+++ src/main/java/com/project/web/SweetCRUD/service/impl/MaintenanceServiceImpl.java
@@ -1,20 +1,29 @@
 package com.project.web.SweetCRUD.service.impl;
 
+import com.project.web.SweetCRUD.dto.CategoryDto;
+import com.project.web.SweetCRUD.dto.ProductCreateDto;
 import com.project.web.SweetCRUD.dto.ProductDTO;
+import com.project.web.SweetCRUD.entity.Category;
 import com.project.web.SweetCRUD.entity.Product;
+import com.project.web.SweetCRUD.repository.CategoryRepository;
 import com.project.web.SweetCRUD.repository.ProductRepository;
 import com.project.web.SweetCRUD.service.MaintenanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MaintenanceServiceImpl implements MaintenanceService {
 
     @Autowired
     ProductRepository productRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @Override
     public List<ProductDTO> findAllProduct() {
@@ -31,5 +40,31 @@ public class MaintenanceServiceImpl implements MaintenanceService {
             products.add(productDto);
         });
         return products;
+    }
+
+    @Override
+    public List<CategoryDto> findAllCategories() {
+        List<CategoryDto> categories = new ArrayList<>();
+        Iterable<Category> iterable = categoryRepository.findAll();
+        for (Category category : iterable) {
+            categories.add(new CategoryDto(category.getId(), category.getName(), category.getDescription()));
+        }
+        return categories;
+    }
+
+    @Override
+    public Boolean createProduct(ProductCreateDto productCreateDto) {
+        Optional<Category> categoryOptional = categoryRepository.findById(productCreateDto.categoryId());
+        if (categoryOptional.isPresent()) {
+            Category category = categoryOptional.get();
+            Product product = new Product();
+            product.setName(productCreateDto.name());
+            product.setPrice(productCreateDto.price());
+            product.setStock(productCreateDto.stock());
+            product.setCategory(category);
+            productRepository.save(product);
+            return true;
+        }
+        return false;
     }
 }
